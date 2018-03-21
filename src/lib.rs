44a2cc879d1e232fc4a5c799c3302cbfe8446677
Yiming Jing <jingyiming@baidu.com>
2018-03-21 13:44:25 -0700

    Nightly detection works
    Change-Id: I05cab7f5d562a4f07256a78e040bc693ff01ec27

--- src/lib.rs
+++ src/lib.rs
@@ -312,8 +312,7 @@
 //! MesaLink is provided under the 3-Clause BSD license. For a copy, see the
 //! LICENSE file.
 
-//#![cfg(feature = "nightly")]
-//#![feature(core_intrinsics)]
+#![cfg_attr(feature = "nightly", feature(core_intrinsics))]
 
 #![deny(trivial_numeric_casts, unused_qualifications)]
 #![forbid(anonymous_parameters, unused_extern_crates, unused_import_braces, unused_results,
