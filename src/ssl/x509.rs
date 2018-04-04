671c9887749f0af00dd5610da8cf866aa60b8b9f
Yiming Jing <jingyiming@baidu.com>
2018-04-03 17:52:51 -0700

    Add x509.rs

---
+++ src/ssl/x509.rs
@@ -0,0 +1,15 @@
+/*
+ *   __  __                 _     _       _
+ *  |  \/  | ___  ___  __ _| |   (_)_ __ | | __
+ *  | |\/| |/ _ \/ __|/ _` | |   | | '_ \| |/ /
+ *  | |  | |  __/\__ \ (_| | |___| | | | |   <
+ *  |_|  |_|\___||___/\__,_|_____|_|_| |_|_|\_\
+ *
+ * Copyright (c) 2017-2018, The MesaLink Authors.
+ * All rights reserved.
+ *
+ * This work is licensed under the terms of the BSD 3-Clause License.
+ * For a copy, see the LICENSE file.
+ *
+ */
+
