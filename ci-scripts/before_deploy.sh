8b35739096e16ed638b91438026dcf50d7bcde0e
Yiming Jing <jingyiming@baidu.com>
2018-08-08 01:34:43 -0700

    Fix before_deploy.sh

--- ci-scripts/before_deploy.sh
+++ ci-scripts/before_deploy.sh
@@ -2,7 +2,7 @@
 
 set -xev
 
-if [[ x"$ARCH" != "x" ] && [ $"TRAVIS_RUST_VERSION" == "stable" ]]
+if [ "$COV" != "yes" ] && [ "$CLIPPY" != "yes" ] && [ "$TRAVIS_RUST_VERSION" == "stable" ]
 then
     mkdir -p $TRAVIS_BUILD_DIR/releases
     cd $TRAVIS_BUILD_DIR/inst
