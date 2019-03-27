68ecc75b24726348045319171f5303f73ad5f305
Yiming Jing <jingyiming@baidu.com>
2019-03-27 12:50:01 -0700

    build: cmake + cdylib builds

--- cmake/CMakeCargo.cmake
+++ cmake/CMakeCargo.cmake
@@ -40,7 +40,15 @@ function(cargo_build)
         set(LIB_BUILD_TYPE "debug")
     endif()
 
-    set(LIB_FILE "${CARGO_TARGET_DIR}/${LIB_TARGET}/${LIB_BUILD_TYPE}/${CMAKE_STATIC_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set(LIB_FILE "${CARGO_TARGET_DIR}/${LIB_TARGET}/${LIB_BUILD_TYPE}/${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set_property(GLOBAL PROPERTY install_lib_file_property "${LIB_FILE}")
+
+    if(CMAKE_SYSTEM_NAME STREQUAL Linux)
+        set(SONAME "${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}${CMAKE_SHARED_LIBRARY_SUFFIX}.${PROJECT_VERSION_MAJOR}")
+    elseif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+        set(SONAME "${CMAKE_SHARED_LIBRARY_PREFIX}${LIB_NAME}.${PROJECT_VERSION_MAJOR}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    endif()
+    set_property(GLOBAL PROPERTY soname_property "${SONAME}")
 
 	if(IOS)
 		set(CARGO_ARGS "lipo")
@@ -55,17 +63,25 @@ function(cargo_build)
 
     file(GLOB_RECURSE LIB_SOURCES "*.rs")
 
-    set(CARGO_ENV_COMMAND ${CMAKE_COMMAND} -E env "CARGO_TARGET_DIR=${CARGO_TARGET_DIR}")
+    if(CMAKE_SYSTEM_NAME STREQUAL Linux)
+        set(CARGO_LINKER_ARGS "-C link-arg=-Wl,-soname -C link-arg=-Wl,${SONAME}" VERBATIM)
+    elseif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+        set(CARGO_LINKER_ARGS "\
+            -C link-arg=-Wl,-install_name -C link-arg=-Wl,${SONAME} \
+            -C link-arg=-Wl,-compatibility_version -C link-arg=-Wl,${PROJECT_VERSION_MAJOR} \
+            -C link-arg=-Wl,-current_version -C link-arg=-Wl,${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}" VERBATIM)
+    endif()
+
+    set(CARGO_ENV_COMMAND ${CMAKE_COMMAND} -E env "CARGO_TARGET_DIR=${CARGO_TARGET_DIR}" "RUSTFLAGS=${CARGO_LINKER_ARGS}")
 
     add_custom_command(
         OUTPUT ${LIB_FILE}
-        COMMAND ${CARGO_ENV_COMMAND} ${CARGO_EXECUTABLE} ARGS ${CARGO_ARGS}
-        COMMAND ${CMAKE_AR} d ${LIB_FILE} floatdisf.o clzsi2.o
+        COMMAND ${CARGO_ENV_COMMAND} ${CARGO_LINKER_ARGS_COMMAND} ${CARGO_EXECUTABLE} ARGS ${CARGO_ARGS}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         DEPENDS ${LIB_SOURCES}
         COMMENT "running cargo")
     add_custom_target(${CARGO_NAME}_target ALL DEPENDS ${LIB_FILE})
-    add_library(${CARGO_NAME} STATIC IMPORTED GLOBAL)
+    add_library(${CARGO_NAME} SHARED IMPORTED GLOBAL)
     add_dependencies(${CARGO_NAME} ${CARGO_NAME}_target)
     set_target_properties(${CARGO_NAME} PROPERTIES IMPORTED_LOCATION ${LIB_FILE})
 endfunction()
