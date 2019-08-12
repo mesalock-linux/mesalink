7a89662f4fbd704c62196f796c338af6d028ddb6
Yiming Jing <jingyiming@baidu.com>
2019-08-12 15:51:52 -0700

    ci: delete the usr/local prefix in release builds

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -11,13 +11,13 @@ fi
 if [[ x"$TARGET" == "x" ]]
 then
     #./autogen.sh --enable-examples
-    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst/usr/local ..
+    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst ..
 else
     rustup target add $RUST_TARGET
     #./autogen.sh --host=$TARGET --enable-rusthost=$RUST_TARGET
-    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst/usr/local -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/cmake/${TARGET}.toolchain.cmake ..
+    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/cmake/${TARGET}.toolchain.cmake ..
 fi
 
 make
 make install
-du -sh $TRAVIS_BUILD_DIR/inst/usr/local/lib/*
+du -sh $TRAVIS_BUILD_DIR/inst/lib/*
