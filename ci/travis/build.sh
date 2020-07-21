3b6a0830b9ae98ce4901e7e128cbe35ae26b4231
Yiming Jing <yjing@apache.org>
2020-07-21 16:33:27 -0700

    Fix failed MinGW CI builds (#49)
    The rust toolchain has changed the file extension for static libs on
    MinGW cross builds: mesalink.lib -> libmesalink.a

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -5,13 +5,16 @@ set -xev
 if [[ x"$TARGET" == "x" ]]
 then
     #./autogen.sh --enable-examples
-    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst ..
+    mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst ..
 else
     rustup target add $RUST_TARGET
     #./autogen.sh --host=$TARGET --enable-rusthost=$RUST_TARGET
-    mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/cmake/${TARGET}.toolchain.cmake ..
+    mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=../inst -DCMAKE_TOOLCHAIN_FILE=${TRAVIS_BUILD_DIR}/cmake/${TARGET}.toolchain.cmake ..
 fi
 
 make
 make install
-du -sh $TRAVIS_BUILD_DIR/inst/lib/*
+if [[ -d "$TRAVIS_BUILD_DIR/inst/lib/" ]]
+then
+    du -sh $TRAVIS_BUILD_DIR/inst/lib/*
+fi
